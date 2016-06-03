45ab9beea07ee9bb8e825524a0add7d1798e033e
Hugo Louro <hmclouro@gmail.com>
2016-06-03 11:26:01 -0700

    STORM-XXXX_KNCAUnitTests  - DONE unit test for KafkaSpout$OffsetEntry with reusable expectations

--- external/storm-kafka-client/src/test/java/org/apache/storm/kafka/spout/KafkaSpoutOffsetEntryTest.java
+++ external/storm-kafka-client/src/test/java/org/apache/storm/kafka/spout/KafkaSpoutOffsetEntryTest.java
@@ -31,104 +31,82 @@ import mockit.Deencapsulation;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
+import mockit.Verifications;
 import mockit.integration.junit4.JMockit;
 
 @RunWith(JMockit.class)
 public class KafkaSpoutOffsetEntryTest {
     @Tested
-    KafkaSpout.OffsetEntry offsetEntry;
+    private KafkaSpout.OffsetEntry offsetEntry;
 
-    @Injectable  KafkaSpout ks;
-    @Injectable  TopicPartition tp;
-    @Injectable("0")  long initialFetchOffset;
-    @Injectable  NavigableSet<KafkaSpoutMessageId> ackedMsgs;
-    @Injectable  KafkaSpoutMessageId kafkaSpoutMessageId;
-    @Injectable  Iterator<KafkaSpoutMessageId> iterator;
+    @Injectable
+    private KafkaSpout ks;
+    @Injectable
+    private TopicPartition tp;
+    @Injectable("0")
+    private long initialFetchOffset;
+    @Injectable
+    private NavigableSet<KafkaSpoutMessageId> ackedMsgs;
+    @Injectable
+    private KafkaSpoutMessageId kafkaSpoutMessageId;
+    @Injectable
+    private Iterator<KafkaSpoutMessageId> iterator;
 
     @Test
-    public void testOffsetEntry(/*@Injectable final KafkaSpout ks,
-                                @Injectable final TopicPartition tp,
-                                @Injectable("0") final long initialFetchOffset,
-                                @Injectable final NavigableSet<KafkaSpoutMessageId> ackedMsgs,
-                                @Injectable final KafkaSpoutMessageId kafkaSpoutMessageId,
-                                @Injectable final Iterator<KafkaSpoutMessageId> iterator*/) throws Exception {
-
+    public void testOffsetEntry() throws Exception {
         Deencapsulation.setField(offsetEntry, ackedMsgs);
 
         // offsets 0-5 have been acked, 6-8 not acked yet, and 9-10 acked out of order
-        new Expectations() {{
-            ackedMsgs.iterator(); result = iterator;
-
-            kafkaSpoutMessageId.offset(); returns(0L, 1L, 2L, 3L, 4L, 5L, 9L, 10L);
-            kafkaSpoutMessageId.getMetadata(Thread.currentThread()); result = "";
-
-            iterator.hasNext(); returns(true, true, true, true, true, true, true, true, false);
-            iterator.next(); returns(kafkaSpoutMessageId);
-        }};
-
-        /*new OffsetExpectations(new Long[]{0L, 1L, 2L, 3L, 4L, 5L, 9L, 10L},
-                new boolean[]{true, true, true, true, true, true, true, true, false});*/
+        new OffsetExpectations(new Long[]{0L, 1L, 2L, 3L, 4L, 5L, 9L, 10L},
+                new boolean[]{true, true, true, true, true, true, true, true, false});
 
         // assert offset 5 is ready to be committed
         OffsetAndMetadata actual = offsetEntry.findNextCommitOffset();
         OffsetAndMetadata expected = new OffsetAndMetadata(5);
         Assert.assertEquals(expected, actual);
 
-        // commit offsets 0-3
+        new Verifications() {{
+            iterator.hasNext(); times = 7;
+            iterator.next(); times = 7;
+        }};
+
+        // commit offsets 0-5
         offsetEntry.commit(actual);
-        /*offsetEntry.commit(new OffsetAndMetadata(3));
-        Assert.assertEquals(new OffsetAndMetadata(4), offsetEntry.findNextCommitOffset());*/
+
+        // assert committed offset is 5
+        long committedOffset = Deencapsulation.getField(offsetEntry, "committedOffset");
+        Assert.assertEquals(5L, committedOffset);
 
         // offsets 7, 9-10 ready, but 6 not yet ready, so next offset to commit should be null
-        new Expectations() {{
-            ackedMsgs.iterator(); result = iterator;
-
-            kafkaSpoutMessageId.offset(); returns(7L, 9L, 10L);
-
-            iterator.hasNext(); returns(true, true, true, false);
-            iterator.next(); returns(kafkaSpoutMessageId);
-        }};
+        // because last committed was 5 we can only commit once 6 is ready
+        new OffsetExpectations(new Long[]{7L, 9L, 10L},
+                new boolean[]{true, true, true, false});
 
         Assert.assertNull(offsetEntry.findNextCommitOffset());
 
+        // assert committed offset is still 5
+        committedOffset = Deencapsulation.getField(offsetEntry, "committedOffset");
+        Assert.assertEquals(5L, committedOffset);
+
         // offsets 6-10 ready, so offset 10 is ready to be committed
-        new Expectations() {{
-            ackedMsgs.iterator(); result = iterator;
-
-            kafkaSpoutMessageId.offset(); returns(6L, 7L, 8L, 9L, 10L);
-            kafkaSpoutMessageId.getMetadata(Thread.currentThread()); result = "";
-
-            iterator.hasNext(); returns(true, true, true, true, true, false);
-            iterator.next(); returns(kafkaSpoutMessageId);
-        }};
+        new OffsetExpectations(new Long[]{6L, 7L, 8L, 9L, 10L},
+                new boolean[]{true, true, true, true, true, false});
 
         actual = offsetEntry.findNextCommitOffset();
         expected = new OffsetAndMetadata(10);
         Assert.assertEquals(expected, actual);
 
-        // after commit no offsets ready
+        // after commit no new offsets ready
         offsetEntry.commit(actual);
         Assert.assertNull(offsetEntry.findNextCommitOffset());
+
+        // assert committed offset is now 10
+        committedOffset = Deencapsulation.getField(offsetEntry, "committedOffset");
+        Assert.assertEquals(10L, committedOffset);
     }
 
-    final class OffsetExpectations extends Expectations {
-        /*private final Long[] offsets;
-        private final boolean[] hasNext;
-        private final NavigableSet<KafkaSpoutMessageId> ackedMsgs;
-        private final KafkaSpoutMessageId kafkaSpoutMessageId;
-        private final Iterator<KafkaSpoutMessageId> iterator;
-
-        public OffsetExpectations(Long[] offsets, boolean[] hasNext, NavigableSet<KafkaSpoutMessageId> ackedMsgs,
-                KafkaSpoutMessageId kafkaSpoutMessageId, Iterator<KafkaSpoutMessageId> iterator) {
-            this.offsets = offsets;
-            this.hasNext = hasNext;
-            this.ackedMsgs = ackedMsgs;
-            this.kafkaSpoutMessageId = kafkaSpoutMessageId;
-            this.iterator = iterator;
-        }*/
-
-        public OffsetExpectations(Long[] offsets, boolean[] hasNext/*, NavigableSet<KafkaSpoutMessageId> ackedMsgs,
-                                  KafkaSpoutMessageId kafkaSpoutMessageId, Iterator<KafkaSpoutMessageId> iterator*/) {
+    private final class OffsetExpectations extends Expectations {
+        OffsetExpectations(Long[] offsets, boolean[] hasNext) {
             ackedMsgs.iterator(); result = iterator;
 
             kafkaSpoutMessageId.offset(); returns(offsets);
